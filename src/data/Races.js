7366b07d7bc867cfe0416d05965ef7bc7efc63a5
denimcouch <alexmata92@ymail.com>
2021-02-10 11:11:28 -0600

    races added

---
+++ src/data/Races.js
@@ -0,0 +1,66 @@
+export const races = [
+  {
+    race: "Human",
+    subraces: [
+      "Calishite",
+      "Chondathan",
+      "Damaran",
+      "Illuskan",
+      "Mulan",
+      "Rashemi",
+      "Shou",
+      "Tethyrian",
+      "Turami"]
+  },
+  {
+    race: "Dragonborn",
+    subraces: [
+      "Red",
+      "Black",
+      "Blue",
+      "White",
+      "Green",
+      "Gold",
+      "Copper",
+      "Silver",
+      "Brass",
+      "Bronze"
+    ]
+  },
+  {
+    race: "Elf",
+    subraces: ["High", "Dark", "Wood"]
+  },
+  {
+    race: "Half-Elf",
+    subraces: ""
+  },
+  {
+    race: "Half-Orc",
+    subraces: ""
+  },
+  {
+    race: "Tiefling",
+    subraces: ["Infernal", "Abyssal"]
+  },
+  {
+    race: "Gnome",
+    subraces: ["Forest", "Rock", "Deep"]
+  },
+  {
+    race: "Genasi",
+    subraces: ["Earth", "Air", "Water", "Fire"]
+  },
+  {
+    race: "Goliath",
+    subraces: ""
+  },
+  {
+    race: "Dwarf",
+    subraces: ["Hill", "Mountain"]
+  },
+  {
+    race: "Firbolg",
+    subraces: ""
+  }
+];

3e2216753f3d42b8ea2eee6a0ccd65babeac530b
denimcouch <alexmata92@ymail.com>
2021-03-16 14:33:04 -0500

    mvp v.1.5

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
   <main class="about">
-    <h1>This is an about page</h1>
+    <h1>CharGen v1.0</h1>
+    <div class="info">
+      <p>
+        CharGen was developed by Alex Mata as a way to help Game Masters in
+        planning their tabletop gaming adventures.
+      </p>
+    </div>
+    <div class="contribute">
+      <h2>Want to contribute?</h2>
+      <p>
+        Join us on the
+        <a href="https://github.com/denimcouch/npc-gen-spa" target="_blank"
+          >CharGen Github</a
+        >.
+      </p>
+    </div>
   </main>
 </template>
+
+<style>
+.about {
+  max-width: 1100px;
+  margin: auto;
+  text-align: left;
+  line-height: 1.5;
+}
+
+.about h1 {
+  margin: 0 0.8rem;
+}
+
+.info {
+  margin: 1rem;
+}
+
+.contribute {
+  margin: 1rem;
+}
+</style>
